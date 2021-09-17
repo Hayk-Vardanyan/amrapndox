8636c4257481d532790fd779bffacbcbd0521e10
HaykVardanyan2004 <90803123+HaykVardanyan2004@users.noreply.github.com>
2021-09-17 22:23:33 +0400

    Add files via upload

---
+++ amr47.cpp
@@ -0,0 +1,31 @@
+#include <iostream> 
+
+std::string foo(std::string str);
+
+int main()
+{
+ std::string str;
+ std::cin >> str;
+  
+ std::cout << foo(str);
+
+
+}
+
+std::string foo(std::string str)
+{		 
+ static int size = str.size() - 1;
+ static std::string res;
+
+if(size < 0) {return res;}
+
+res += str[size];
+--size;
+
+  if(size < 0) {return res;}
+   
+  return foo(str);
+
+
+}
+
